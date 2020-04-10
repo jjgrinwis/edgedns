1344b5effae9ce4abe4f88b74cf6e77f564154a8
John Grinwis <john.grinwis@gmail.com>
2020-04-10 22:06:20 +0200

    test with AKAMAICDN

--- grinwis_com.tf
+++ grinwis_com.tf
@@ -3,8 +3,8 @@ resource "akamai_dns_zone" "grinwis_com" {
   contract       = data.akamai_contract.default.id
   group          = data.akamai_group.default.id
   zone           = "grinwis.com"
-  masters        = [""]
-  type           = "PRIMARY"
+  type           = "secondary"
+  masters        = ["1.2.3.4", "1.2.3.5"]
   comment        = "zone created via terraform"
   sign_and_serve = false
 }
@@ -12,9 +12,9 @@ resource "akamai_dns_zone" "grinwis_com" {
 resource "akamai_dns_record" "www_grinwis_com" {
   zone       = "grinwis.com"
   name       = "www.grinwis.com"
-  recordtype = "CNAME"
+  recordtype = "AKAMAICDN"
   active     = true
   ttl        = 600
-  target     = ["www.grinwis.com.edgesuite.net"]
+  target     = ["www.grinwis.com"]
   depends_on = [akamai_dns_zone.grinwis_com]
 }
